c73d843542c8b2edd059b2d03dec37e75ba5fc09
prajwal078 <prajwal078@gmail.com>
2018-12-28 18:54:11 +1100

    Updates

---
+++ precache-manifest.045c2a89ced3aba20338911c807bda6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2b780d93f52bfd790bc",
+    "url": "/react-number2words-demo/static/js/runtime~main.f2b780d9.js"
+  },
+  {
+    "revision": "379f66023474dd1fc070",
+    "url": "/react-number2words-demo/static/js/main.379f6602.chunk.js"
+  },
+  {
+    "revision": "76520827a893628815fb",
+    "url": "/react-number2words-demo/static/js/1.76520827.chunk.js"
+  },
+  {
+    "revision": "379f66023474dd1fc070",
+    "url": "/react-number2words-demo/static/css/main.b3e99c5e.chunk.css"
+  },
+  {
+    "revision": "ec0b5304adcf8815d4f074a1feef650b",
+    "url": "/react-number2words-demo/index.html"
+  }
+];
